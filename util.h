295b7c587ee4b2c4318f5b1fcd5dc24d5eb5de68
Tobias Schramm <tobleminer@gmail.com>
2018-01-03 03:58:16 +0100

    Don't define offsetof

--- util.h
+++ util.h
@@ -1,9 +1,8 @@
 #ifndef _SHORELINE_UTIL_H_
 #define _SHORELINE_UTIL_H_
 
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 #define container_of(ptr, type, member) ({ \
 	const typeof(((type *)0)->member) * __mptr = (ptr); \
-	(type *)((char *)__mptr - offsetof(type, member)); })
+	(type *)((char *)__mptr - ((size_t) &((type *)0)->member)); })
 
 #endif
