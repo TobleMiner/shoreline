48fcc800dace3d5986105e10acf397bd6bdde2ee
Tobias Schramm <tobleminer@gmail.com>
2018-05-22 16:58:12 +0200

    Free threads only if they were allocated

--- network.c
+++ network.c
@@ -52,7 +52,7 @@ static int one = 1;
 
 int net_alloc(struct net** network, struct fb* fb, size_t ring_size) {
 	int err = 0;
-	struct net* net = malloc(sizeof(struct net));
+	struct net* net = calloc(1, sizeof(struct net));
 	if(!net) {
 		err = -ENOMEM;
 		goto fail;
@@ -72,7 +72,9 @@ fail:
 
 void net_free(struct net* net) {
 	assert(net->state == NET_STATE_EXIT || net->state == NET_STATE_IDLE);
-	free(net->threads);
+	if(net->threads) {
+		free(net->threads);
+	}
 	free(net);
 }
 
